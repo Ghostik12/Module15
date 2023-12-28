730cfc0f41e68382d0c0d45a194dc470e4cf12d3
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-12-28 22:22:59 +0300

    Task 15.4.1

---
+++ Module15/Module15/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace Module15
+{
+    internal class Department
+    {
+        public Department()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Module15/Module15/Program.cs
+++ Module15/Module15/Program.cs
@@ -7,50 +7,31 @@ namespace Module15
     {
         static void Main(string[] args)
         {
-            var phoneBook = new List<Contact>() 
-            { 
-                new Contact("Игорь", 79990000001, "igor@example.com"),
-                new Contact("Сергей", 79990000010, "serge@example.com"),
-                new Contact("Анатолий", 79990000011, "anatoly@example.com"),
-                new Contact("Валерий", 79990000012, "valera@example.com"),
-                new Contact("Сергей", 799900000013, "serg@gmail.com"),
-                new Contact("Иннокентий", 799900000013, "innokentii@example.com")
+            var departments = new List<Department>()
+            {
+                new Department() {Id = 1, Name = "Программирование"},
+                new Department() {Id = 2, Name = "Продажи"}
+            };
+            var employees = new List<Employee>()
+            {
+                new Employee() { DepartmentId = 1, Name = "Инна", Id = 1},
+                new Employee() { DepartmentId = 1, Name = "Андрей", Id = 2},
+                new Employee() { DepartmentId = 2, Name = "Виктор", Id = 3},
+                new Employee() { DepartmentId = 3, Name = "Альберт ", Id = 4},
             };
 
-            var checkEmail = phoneBook.GroupBy(x => x.Email.Contains("example"));
+            var result = employees.Join(departments,
+                employee => employee.DepartmentId,
+                dep => dep.Id,
+                (employee, dep) =>
+                new
+                {
+                    EmployeeName = employee.Name,
+                    DepartmentName = dep.Name
+                });
 
-            foreach (var contact in checkEmail)
-            {
-                Console.WriteLine(contact.Key);
-
-                foreach (var contact2 in contact)
-                    Console.WriteLine(contact2.Name);
-            }
-
-            ////  в качестве критерия группировки передаем домен адреса электронной почты
-            //var grouped = phoneBook.GroupBy(c => c.Email.Split("@").Last());
-
-            //// обрабатываем получившиеся группы
-            //foreach (var group in grouped)
-            //{
-            //    // если ключ содержит example, значит, это фейк
-            //    if (group.Key.Contains("example"))
-            //    {
-            //        Console.WriteLine("Фейковые адреса: ");
-
-            //        foreach (var contact in group)
-            //            Console.WriteLine(contact.Name + " " + contact.Email);
-
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Реальные адреса: ");
-            //        foreach (var contact in group)
-            //            Console.WriteLine(contact.Name + " " + contact.Email);
-            //    }
-
-            //    Console.WriteLine();
-            //}
+            foreach (var department in result)
+                Console.WriteLine(department.EmployeeName + " - " + department.DepartmentName);
         }
     }
 }
